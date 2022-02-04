ba2e32cf99f0d60d990df6e0b053375e9e71dca4
Job79 <jobse@pm.me>
2022-02-04 20:21:06 +0100

    Use hash as file name on publish and remove comments default config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[hash].js`,
+        chunkFileNames: `assets/[hash].js`,
+        assetFileNames: `assets/[hash].[ext]`,
+      },
+    },
+  },
 });
