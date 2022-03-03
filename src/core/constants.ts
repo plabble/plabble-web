85ca537d7350a3c71028838aa0d9a40c64336bcf
Maurice <mauricegolverdingen@hotmail.com>
2022-03-03 14:57:38 +0100

    started with responsive gui 2

---
+++ src/core/constants.ts
@@ -0,0 +1,7 @@
+export const VERSION = 1;
+
+export const IDENTIFIER_SIZE = 20;
+export const SALT_SIZE = 16;
+export const SIGNATURE_SIZE = 64;
+export const MAC_SIZE = 16;
+export const KEY_SIZE = 32;
---
+++ src/lib/navigation/SideNav.svelte
@@ -0,0 +1,82 @@
+<style lang="scss">
+  @import '../../assets/scss/_variables.scss';
+
+  .sidenav {
+    position: absolute;
+    height: 100%;
+    left: 0;
+    top: 0;
+    z-index: 1000;
+    background-color: var(--card-background-color);
+
+    @if map-get($breakpoints, 'sm') {
+      @media (max-width: map-get($breakpoints, 'md')) {
+        width: 80%;
+        max-width: 80%;
+      }
+    }
+
+    @if map-get($breakpoints, 'md') {
+      @media (min-width: map-get($breakpoints, 'md')) {
+        width: 40%;
+        max-width: 40%;
+      }
+    }
+
+    @if map-get($breakpoints, 'lg') {
+      @media (min-width: map-get($breakpoints, 'lg')) {
+        width: 30%;
+        max-width: 30%;
+      }
+    }
+
+    @if map-get($breakpoints, 'xl') {
+      @media (min-width: map-get($breakpoints, 'xl')) {
+        width: 25%;
+        max-width: 25%;
+      }
+    }
+  }
+
+  .sidenav-bg {
+    //pointer-events: none; //can click through it
+    background-color: rgba($color: #000, $alpha: 0.6);
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 999;
+  }
+</style>
+
+<script lang="ts">
+  import { fly } from 'svelte/transition';
+  import { swipe } from 'svelte-gestures';
+
+  export let open = false;
+
+  // Swipe left to close menu (on touch devices)
+  function handleSwipe(event) {
+    if (event.detail?.direction === 'left') {
+      open = false;
+    }
+  }
+</script>
+
+{#if open}
+  <div
+    on:click|self="{() => (open = false)}"
+    use:swipe="{{ minSwipeDistance: 100, timeframe: 300, touchAction: 'none' }}"
+    on:swipe="{handleSwipe}"
+    class="sidenav-bg"
+  >
+    <div
+      in:fly="{{ duration: 600, x: -500, opacity: 1 }}"
+      out:fly="{{ duration: 600, x: -500, opacity: 1 }}"
+      class="sidenav"
+    >
+      <slot />
+    </div>
+  </div>
+{/if}
